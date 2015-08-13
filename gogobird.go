eb71e74fc5a53614a7e5b2dbcc09751d8c2d9f3e
choueric <zhssmail@gmail.com>
2015-08-13 22:42:38 +0200

    add post tweet and get user function.

--- gogobird.go
+++ gogobird.go
@@ -11,78 +11,14 @@ import (
 	"github.com/ChimeraCoder/anaconda"
 )
 
+type User anaconda.User
+
 var CONSUMER_KEY = os.Getenv("CONSUMER_KEY")
 var CONSUMER_SECRET = os.Getenv("CONSUMER_SECRET")
 var ACCESS_TOKEN = os.Getenv("ACCESS_TOKEN")
 var ACCESS_TOKEN_SECRET = os.Getenv("ACCESS_TOKEN_SECRET")
 
 var api *anaconda.TwitterApi
-
-// Test_TwitterCredentials tests that non-empty Twitter credentials are set
-// Without this, all following tests will fail
-func testCredentials() bool {
-	if CONSUMER_KEY == "" || CONSUMER_SECRET == "" || ACCESS_TOKEN == "" || ACCESS_TOKEN_SECRET == "" {
-		fmt.Printf("Credentials are invalid: at least one is empty")
-		return false
-	}
-	return true
-}
-
-// Test that creating a TwitterApi client creates a client with non-empty OAuth credentials
-func Test_TwitterApi_NewTwitterApi(t *testing.T) {
-	anaconda.SetConsumerKey(CONSUMER_KEY)
-	anaconda.SetConsumerSecret(CONSUMER_SECRET)
-	api = anaconda.NewTwitterApi(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-
-	if api.Credentials == nil {
-		t.Errorf("Twitter Api client has empty (nil) credentials")
-	}
-}
-
-// Test that the GetSearch function actually works and returns non-empty results
-func Test_TwitterApi_GetSearch(t *testing.T) {
-	search_result, err := api.GetSearch("golang", nil)
-	if err != nil {
-		t.Errorf("GetSearch yielded error %s", err.Error())
-		panic(err)
-	}
-
-	// Unless something is seriously wrong, there should be at least two tweets
-	if len(search_result.Statuses) < 2 {
-		t.Errorf("Expected 2 or more tweets, and found %d", len(search_result.Statuses))
-	}
-
-	// Check that at least one tweet is non-empty
-	for _, tweet := range search_result.Statuses {
-		if tweet.Text != "" {
-			return
-		}
-		fmt.Print(tweet.Text)
-	}
-
-	t.Errorf("All %d tweets had empty text", len(search_result.Statuses))
-}
-
-// Test that a valid user can be fetched
-// and that unmarshalling works properly
-func Test_GetUser(t *testing.T) {
-	const username = "chimeracoder"
-
-	users, err := api.GetUsersLookup(username, nil)
-	if err != nil {
-		t.Errorf("GetUsersLookup returned error: %s", err.Error())
-	}
-
-	if len(users) != 1 {
-		t.Errorf("Expected one user and received %d", len(users))
-	}
-
-	// If all attributes are equal to the zero value for that type,
-	// then the original value was not valid
-	if reflect.DeepEqual(users[0], anaconda.User{}) {
-		t.Errorf("Received %#v", users[0])
-	}
-}
 
 func Test_GetFavorites(t *testing.T) {
 	v := url.Values{}
@@ -259,16 +195,104 @@ func Test_TwitterApi_Throttling(t *testing.T) {
 	api.SetDelay(oldDelay)
 }
 
-func Test_DMScreenName(t *testing.T) {
+/***************************************************************************/
+func getDMScreenName() {
 	to, err := api.GetSelf(url.Values{})
 	if err != nil {
-		t.Error(err)
+		fmt.Printf("error: %v\n", err)
+		return
 	}
 	_, err = api.PostDMToScreenName("Test the anaconda lib", to.ScreenName)
 	if err != nil {
-		t.Error(err)
+		fmt.Printf("error: %v\n", err)
 		return
 	}
+}
+
+func postTweet(post string) bool {
+	_, err := api.PostTweet(post, url.Values{})
+	if err != nil {
+		fmt.Printf("post tweet failed: %v\n", err)
+		return false
+	}
+	return true
+}
+
+func (user User) String() string {
+	return fmt.Sprintf(" ContributorsEnabled: %v. CreatedAt: %v.\n DefaultProfile: %v. "+
+		" DefaultProfileImage: %v.\n Description: %v\n Entities: %v\n"+
+		" FavouritesCount: %v. FollowRequestSent: %v. FollowersCount: %v."+
+		" Following: %v. FriendsCount: %v.\n GeoEnabled: %v. Id: %v. IdStr: %v.\n"+
+		" IsTranslator: %v. Lang: %v. ListedCount: %v. Location: %v. Name: %v.\n"+
+		" Notifications: %v. ProfileBackgroundColor: %v.\n ProfileBackgroundImageURL: %v\n"+
+		" ProfileBackgroundImageUrlHttps: %v\n ProfileBackgroundTile: %v\n"+
+		" ProfileBannerURL: %v\n ProfileImageURL: %v\n ProfileImageUrlHttps: %v\n"+
+		" ProfileLinkColor: %v. ProfileSidebarBorderColor: %v.\n"+
+		" ProfileSidebarFillColor: %v. ProfileTextColor: %v."+
+		" ProfileUseBackgroundImage: %v\n Protected: %v. ScreenName: %v\n"+
+		" ShowAllInlineMedia: %v.\n Status: %v\n StatusesCount: %v."+
+		" TimeZone: %v. URL: %v\n UtcOffset: %v. Verified: %v."+
+		" WithheldInCountries: %v. WithheldScope: %v\n",
+		user.ContributorsEnabled, user.CreatedAt, user.DefaultProfile, user.DefaultProfileImage, user.Description,
+		user.Entities, user.FavouritesCount, user.FollowRequestSent, user.FollowersCount,
+		user.Following, user.FriendsCount, user.GeoEnabled, user.Id, user.IdStr,
+		user.IsTranslator, user.Lang, user.ListedCount, user.Location, user.Name,
+		user.Notifications, user.ProfileBackgroundColor, user.ProfileBackgroundImageURL,
+		user.ProfileBackgroundImageUrlHttps, user.ProfileBackgroundTile, user.ProfileBannerURL,
+		user.ProfileImageURL, user.ProfileImageUrlHttps, user.ProfileLinkColor,
+		user.ProfileSidebarBorderColor, user.ProfileSidebarFillColor, user.ProfileTextColor,
+		user.ProfileUseBackgroundImage, user.Protected, user.ScreenName, user.ShowAllInlineMedia,
+		user.Status, user.StatusesCount, user.TimeZone, user.URL, user.UtcOffset, user.Verified,
+		user.WithheldInCountries, user.WithheldScope)
+}
+
+// Test that a valid user can be fetched
+// and that unmarshalling works properly
+func getUser(username string) bool {
+	users, err := api.GetUsersLookup(username, nil)
+	if err != nil {
+		fmt.Printf("GetUsersLookup returned error: %s", err.Error())
+		return false
+	}
+
+	if len(users) != 1 {
+		fmt.Printf("Expected one user and received %d", len(users))
+	}
+
+	// If all attributes are equal to the zero value for that type,
+	// then the original value was not valid
+	if reflect.DeepEqual(users[0], anaconda.User{}) {
+		fmt.Printf("invalid user received\n")
+		return false
+	}
+
+	for i, user := range users {
+		fmt.Printf("---- [%d] ----\n%v\n", i, User(user))
+	}
+	return true
+}
+
+// Test_TwitterCredentials tests that non-empty Twitter credentials are set
+// Without this, all following tests will fail
+func testCredentials() bool {
+	if CONSUMER_KEY == "" || CONSUMER_SECRET == "" || ACCESS_TOKEN == "" || ACCESS_TOKEN_SECRET == "" {
+		fmt.Printf("Credentials are invalid: at least one is empty")
+		return false
+	}
+	return true
+}
+
+// Test that creating a TwitterApi client creates a client with non-empty OAuth credentials
+func newTwitterApi() *anaconda.TwitterApi {
+	anaconda.SetConsumerKey(CONSUMER_KEY)
+	anaconda.SetConsumerSecret(CONSUMER_SECRET)
+	api = anaconda.NewTwitterApi(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+	if api.Credentials == nil {
+		fmt.Printf("Twitter Api client has empty (nil) credentials")
+		return nil
+	}
+	return api
 }
 
 func trySearch(api *anaconda.TwitterApi, topic string) {
@@ -290,8 +314,6 @@ func trySearch(api *anaconda.TwitterApi, topic string) {
 			fmt.Println(i, tweet.Text)
 		}
 	}
-
-	fmt.Printf("All %d tweets had empty text\n", len(search_result.Statuses))
 }
 
 func main() {
@@ -299,15 +321,16 @@ func main() {
 		return
 	}
 
-	anaconda.SetConsumerKey(CONSUMER_KEY)
-	anaconda.SetConsumerSecret(CONSUMER_SECRET)
-	api = anaconda.NewTwitterApi(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-
-	if api.Credentials == nil {
+	if api = newTwitterApi(); api == nil {
 		fmt.Printf("Twitter Api client has empty (nil) credentials")
 		return
 	}
 
-	trySearch(api, "天津")
-
+	//trySearch(api, "天津")
+	//getUser("chou_eric")
+	if postTweet("test from gogobird") == true {
+		fmt.Printf("post tweet success.\n")
+	} else {
+		fmt.Printf("post tweet fail.\n")
+	}
 }
